4deb8e679e7033e5c4ec8952da0f1df878932a8d
Mohd Atif <mohdatif95466@gmail.com>
2024-08-08 23:26:39 +0530

    Initial commit

---
+++ product-crud/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// routes
+app.use('/api/products', productRoute);
+
+// start express at the given port
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// mongodb(docker) connection
+mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+  .then(() => console.log('Connected!'))
+  .catch((e) => console.log('MongoDB not connected ' + e));
